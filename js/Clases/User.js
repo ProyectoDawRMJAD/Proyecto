6c652b4512aef88176402c4526fd86b840274e12
MoiRicHer <kzelda19@gmail.com>
2024-12-03 09:41:36 +0100

    Elimino direccion y cambio atributos de user

--- js/Clases/User.js
+++ js/Clases/User.js
@@ -1,18 +1,21 @@
 import { Direccion } from "./Direccion.js";
 import { Empresa } from "./Empresa.js";
 
-class User{
-    constructor(id,name,username,email,street,suite,city,zipcode,lat,lng,phone,website,companyName,catchPhrase,bs){
+class User extends HTMLElement{
+    //Constructor User
+    constructor(id,name,username,email,phone,website,companyName,catchPhrase,bs){
         this.id = id;
         this.name = name;
         this.username = username;
         this.email = email;
-        this.direccion = new Direccion(street,suite,city,zipcode,lat,lng);
         this.phone = phone;
         this.website = website;
         this.empresa = new Empresa(companyName,catchPhrase,bs);
-
     }
+    
+
+
+
 }
 export{
     User
