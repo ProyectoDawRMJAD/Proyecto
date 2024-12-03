981cecc876f324a6629c35b057f4a3141b851697
RaulJuanMarti <raujuamar2@alu.edu.gva.es>
2024-12-03 10:06:39 +0100

    asd

--- js/main.js
+++ js/main.js
@@ -3,6 +3,25 @@ import { Foto } from "./Clases/Foto.js";
 import { Post } from "./Clases/Post.js";
 import { Tarea } from "./Clases/Tarea.js";
 import { User } from "./Clases/User.js";
+<<<<<<< HEAD
+import {fotos} from "/json/photos.js";
+import {posts} from "/json/posts.js";
+import {todos} from "/json/todos.js";
+import {users} from "/json/users.js";
+import {comments} from "/json/comments.js";
+console.log(fotos);
+console.log(posts);
+console.log(todos);
+console.log(users);
+console.log(comments);
 
+customElements.define("template-user",User);
+customElements.define("template-comentario",Comentario);
+customElements.define("template-post",Post);
+customElements.define("template-todo",Tarea);
+customElements.define("template-foto",Foto);
 
+=======
+
+>>>>>>> 3823b980a2b2055ba1fce5cedb4e4bedd29863aa
 
