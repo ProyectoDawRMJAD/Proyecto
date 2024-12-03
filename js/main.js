01e5315b1ed53e3470fe811b95d6e6e72d8b91f0
RaulJuanMarti <raujuamar2@alu.edu.gva.es>
2024-12-03 10:07:23 +0100

    sdf

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ import { Foto } from "./Clases/Foto.js";
 import { Post } from "./Clases/Post.js";
 import { Tarea } from "./Clases/Tarea.js";
 import { User } from "./Clases/User.js";
-<<<<<<< HEAD
 import {fotos} from "/json/photos.js";
 import {posts} from "/json/posts.js";
 import {todos} from "/json/todos.js";
@@ -20,8 +19,4 @@ customElements.define("template-comentario",Comentario);
 customElements.define("template-post",Post);
 customElements.define("template-todo",Tarea);
 customElements.define("template-foto",Foto);
-
-=======
-
->>>>>>> 3823b980a2b2055ba1fce5cedb4e4bedd29863aa
 
