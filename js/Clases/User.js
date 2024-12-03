3823b980a2b2055ba1fce5cedb4e4bedd29863aa
MoiRicHer <kzelda19@gmail.com>
2024-12-03 09:54:29 +0100

    Cambios Plantilla

--- js/Clases/User.js
+++ js/Clases/User.js
@@ -4,6 +4,7 @@ import { Empresa } from "./Empresa.js";
 class User extends HTMLElement{
     //Constructor User
     constructor(id,name,username,email,phone,website,companyName,catchPhrase,bs){
+        super();
         this.id = id;
         this.name = name;
         this.username = username;
@@ -11,7 +12,19 @@ class User extends HTMLElement{
         this.phone = phone;
         this.website = website;
         this.empresa = new Empresa(companyName,catchPhrase,bs);
+
     }
+    connectedCallBack(){
+        let shadow=this.attachShadow({mode:"open"});
+        let plantilla=document.getElementById("user");
+        let contenido=plantilla.content;
+        let user=contenido.cloneNode(true);
+        user.querySelector("#titulo").textContent=this.name;
+
+        shadow.append(user);
+
+    }
+
     
 
 
--- js/main.js
+++ js/main.js
@@ -4,5 +4,9 @@ import { Post } from "./Clases/Post.js";
 import { Tarea } from "./Clases/Tarea.js";
 import { User } from "./Clases/User.js";
 
-
+customElements.define("template-user",User);
+customElements.define("template-comentario",Comentario);
+customElements.define("template-post",Post);
+customElements.define("template-todo",Tarea);
+customElements.define("template-foto",Foto);
 
