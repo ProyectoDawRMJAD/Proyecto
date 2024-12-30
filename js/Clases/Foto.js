56f111619519de91413679a2c4db026783b09632
JesusKuro <jesuscanicio33@gmail.com>
2024-12-30 17:30:02 +0100

    Botones De Eliminar

--- js/Clases/Foto.js
+++ js/Clases/Foto.js
@@ -1,5 +1,5 @@
-class Foto extends HTMLElement{
-    constructor(albumId,id,title,url,thumbnail){
+class Foto extends HTMLElement {
+    constructor(albumId, id, title, url, thumbnail) {
         super();
         this.albumId = albumId;
         this.id = id;
@@ -7,31 +7,60 @@ class Foto extends HTMLElement{
         this.url = url;
         this.thumbnail = thumbnail;
     }
-    connectedCallback(){
-        if(!this.shadowRoot){
-            let shadow = this.attachShadow({mode:"open"});
+
+    connectedCallback() {
+        if (!this.shadowRoot) {
+            let shadow = this.attachShadow({ mode: "open" });
             let plantilla = document.getElementById("foto");
             let estilo = document.createElement("link");
-            estilo.setAttribute("rel","stylesheet");
-            estilo.setAttribute("href","./css/foto.css");
-    
+            estilo.setAttribute("rel", "stylesheet");
+            estilo.setAttribute("href", "./css/foto.css");
+
             let contenido = plantilla.content;
             let clon = contenido.cloneNode(true);
             clon.querySelector("#numId").textContent = this.id;
-            clon.querySelector("#nombre").textContent = this.title; 
+            clon.querySelector("#nombre").textContent = this.title;
 
             let imagen = clon.querySelector("#imagen");
             imagen.src = this.url;
-            imagen.onerror = ()=>{
+            imagen.onerror = () => {
                 imagen.src = "/img/error.png";
-            }
+            };
+
+            // Modal de confirmación
+            let modal = document.createElement("div");
+            modal.setAttribute("id", "modalEliminarFoto");
+            modal.setAttribute("class", "modal");
+            modal.innerHTML = `
+                <div class="modal-content">
+                    <p>¿Estás seguro de que deseas eliminar esta imagen?</p>
+                    <button id="confirmarEliminarFoto">Sí</button>
+                    <button id="cancelarEliminarFoto">No</button>
+                </div>
+            `;
+            shadow.appendChild(modal);
+
+            // Botón eliminar
+            let btnEliminarFoto = clon.querySelector("#btnEliminarFoto");
+            btnEliminarFoto.addEventListener("click", () => {
+                modal.style.display = "block";
+            });
+
+            // Confirmar eliminación
+            modal.querySelector("#confirmarEliminarFoto").addEventListener("click", () => {
+                this.remove();
+                modal.style.display = "none";
+            });
+
+            // Cancelar eliminación
+            modal.querySelector("#cancelarEliminarFoto").addEventListener("click", () => {
+                modal.style.display = "none";
+            });
 
             shadow.appendChild(estilo);
             shadow.appendChild(clon);
         }
-       
     }
 }
-export{
-    Foto
-}
+
+export { Foto };
